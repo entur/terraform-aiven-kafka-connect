91e1ddf6e7c332d0ab953a770deb0b9f8681ddf3
Upendra Vedullapalli <upendra.rao.vedullapalli@entur.org>
2022-10-20 14:48:11 +0200

    fix: update provider versions

--- modules/bigquery-sink/versions.tf
+++ modules/bigquery-sink/versions.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aiven = {
       source = "aiven/aiven"
+      version = ">= 3.0.0, < 4.0.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = ">=4.39.0"
+      version = ">=4.40.0"
     }
   }
   required_version = ">=0.13.2"
