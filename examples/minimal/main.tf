e7ea48c4f5e110512ad61d085cc3eed9093dda8f
Upendra Vedullapalli <upendra.rao.vedullapalli@entur.org>
2023-03-25 11:09:36 +0100

    feat: Option to use either SA key from module input or create based on SA id. Kafka user configuration is now part of sink module.

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -4,7 +4,7 @@ module "init" {
   # This is an example only; if you're adding this block to a live configuration,
   # make sure to use the latest release of the init module, found here:
   # https://github.com/entur/terraform-aiven-kafka-connect-init/releases
-  source       = "github.com/entur/terraform-aiven-kafka-connect-init//modules/init?ref=v1.1.2"
+  source       = "github.com/entur/terraform-aiven-kafka-connect-init//modules/init?ref=v1.1.3"
   access_token = var.my_aiven_access_token
   project_name = "my-aiven-project-name"
   service_name = "my-aiven-kafka-connect-service-name"
@@ -18,7 +18,8 @@ module "bigquery-sink" {
   connector_name        = "my-connector-name"
   bigquery_project_name = "my-bigquery-project-name"
   bigquery_dataset_name = "my-bigquery-dataset-name"
-  service_account_id    = "my-service-account-id"
+  service_account_id    = "my-service-account-id@my-project.iam.gserviceaccount.com"
+  kafka_username        = "my-kafka-user-name"
   kafka_topics          = ["my-topic-1", "my-topic-2"]
   additional_configuration = {
     "any.other.config.parameter" : "any-other-config-parameter-value"
